edcef004f5eec1f923efcdff5ae2d35f269a61b0
kimgiraffe <79824230+kimgiraffe@users.noreply.github.com>
2023-03-30 12:45:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,3 @@ print("Hello World 2 in team leader server")
 print("Hello World 3 in team member A local server")
 
 print("Hello World 4 in team member B local server")
-
-print("Hello World 5 in team member C local server")
